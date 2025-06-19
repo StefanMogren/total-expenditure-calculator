cd5dc592ad1698d3870cd01968cbb37c534d1c99
StefanMogren <stefan_mogren@hotmail.com>
2025-06-19 09:49:15 +0200

    Created expenditure Schema model

---
+++ api/models/expenditure.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const expenditureSchema = new Schema(
+	{
+		purchase: {
+			type: String,
+			required: true,
+		},
+		when: {
+			type: String,
+			required: true,
+		},
+		amount: {
+			type: Number,
+			required: true,
+			min: 1,
+		},
+		who: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Expenditure = mongoose.model("Expenditure", expenditureSchema);
+
+export default Expenditure;
