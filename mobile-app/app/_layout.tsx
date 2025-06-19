0a8597b52eb771153813b644a246e842964f881d
StefanMogren <stefan_mogren@hotmail.com>
2025-06-19 09:00:13 +0200

    Added basic tab navigation, moved pages into (tabs)

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -3,8 +3,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
 	return (
 		<Stack>
+			<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+			{/* 
+			Så som det stod innan jag skapade "(tabs)"-mappen.
 			<Stack.Screen name='index' options={{ title: "Home" }} />
-			<Stack.Screen name='about' options={{ title: "About" }} />
+			<Stack.Screen name='about' options={{ title: "About" }} /> */}
 		</Stack>
 	);
 }
